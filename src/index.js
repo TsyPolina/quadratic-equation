45ee8b32d499830144d795bcbd56bb987082d6b2
TsyPolina <tsy.polina@gmail.com>
2018-09-21 03:48:48 +0300

    4 task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let x1, x2, discriminant;
+  let array_input = [];
+  let need_str = equation.replace(/\s/g, '').replace(/x\^2|x/g, '');
+  array_input = need_str.split('*');
+  let a = array_input[0];
+  let b = array_input[1];
+  let c = array_input[2];
+  discriminant = b * b - 4 * a * c;
+  x1 = Math.round((-b + Math.sqrt(discriminant)) / (2 * a));
+  x2 = Math.round((-b - Math.sqrt(discriminant)) / (2 * a));
+  if (x1 > x2) {
+    return [x2, x1];
+  }
+  else {
+    return [x1, x2];
+  }
 }
